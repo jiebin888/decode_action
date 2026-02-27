2a860dcc6b8273b091d14e9911e8cc8ef68d014d
action <action@github.com>
2026-02-27 02:00:03 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,4 +1,4 @@
-//Fri Feb 27 2026 01:58:53 GMT+0000 (Coordinated Universal Time)
+//Fri Feb 27 2026 02:00:03 GMT+0000 (Coordinated Universal Time)
 //Base:<url id="cv1cref6o68qmpt26ol0" type="url" status="parsed" title="GitHub - echo094/decode-js: JS混淆代码的AST分析工具 AST analysis tool for obfuscated JS code" wc="2165">https://github.com/echo094/decode-js</url>
 //Modify:<url id="cv1cref6o68qmpt26olg" type="url" status="parsed" title="GitHub - smallfawn/decode_action: 世界上本来不存在加密，加密的人多了，也便成就了解密" wc="741">https://github.com/smallfawn/decode_action</url>
 function GetData01(_0x495200) {
@@ -20,33 +20,27 @@ function escapeRegExp(_0x4ebec3) {
 }
 function UrlDecBase64(_0x105814) {
   for (const [_0x319fb0, _0x4b7aa6] of aaa) {
-    {
-      _0x105814 = _0x105814.replaceAll(_0x4b7aa6, _0x319fb0);
-    }
+    _0x105814 = _0x105814.replaceAll(_0x4b7aa6, _0x319fb0);
   }
   const _0x5472d3 = CryptoJS.enc.Base64.parse(_0x105814);
   return CryptoJS.enc.Utf8.stringify(_0x5472d3);
 }
 function aesCbcDecrypt(_0x1c4fcc) {
   try {
-    {
-      const _0x288ab8 = CryptoJS.enc.Base64.parse(_0x1c4fcc);
-      const _0x351ddb = CryptoJS.enc.Utf8.parse(CryptoJS.MD5(window.location.origin).toString());
-      const _0x3219c9 = CryptoJS.enc.Utf8.parse("cAnByjySGOstUlMc".padEnd(16, ""));
-      const _0x441835 = CryptoJS.AES.decrypt({
-        ciphertext: _0x288ab8
-      }, _0x351ddb, {
-        iv: _0x3219c9,
-        mode: CryptoJS.mode.CBC,
-        padding: CryptoJS.pad.Pkcs7
-      });
-      const _0x22dd27 = _0x441835.toString(CryptoJS.enc.Utf8);
-      return _0x22dd27;
-    }
+    const _0x288ab8 = CryptoJS.enc.Base64.parse(_0x1c4fcc);
+    const _0x351ddb = CryptoJS.enc.Utf8.parse(CryptoJS.MD5(window.location.origin).toString());
+    const _0x3219c9 = CryptoJS.enc.Utf8.parse("cAnByjySGOstUlMc".padEnd(16, ""));
+    const _0x441835 = CryptoJS.AES.decrypt({
+      ciphertext: _0x288ab8
+    }, _0x351ddb, {
+      iv: _0x3219c9,
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7
+    });
+    const _0x22dd27 = _0x441835.toString(CryptoJS.enc.Utf8);
+    return _0x22dd27;
   } catch (_0x499d5e) {
-    {
-      return "";
-    }
+    return "";
   }
 }
 encode_version = "jsjiami.com.v5";
